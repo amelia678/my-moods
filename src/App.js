7876a3edcd5bd51489e143c900553080918ccee0
Amelia Schulz <amelia.schulz@gmail.com>
2020-10-13 18:25:06 -0400

    Add form component with cycle start date.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MoodForm from "./MoodForm/MoodForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h3>My Moods</h3>
+      <MoodForm />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
