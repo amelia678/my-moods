6308755866de64e4133b3c8238e73b5be3db0bac
Amelia Schulz <amelia.schulz@gmail.com>
2020-10-16 17:03:48 -0400

    Add mood and anxiety inputs.

--- src/MoodForm/MoodForm.tsx
+++ src/MoodForm/MoodForm.tsx
@@ -4,6 +4,8 @@ import moment from "moment";
 const MoodForm = () => {
   const [start, setStart] = useState("");
   const [today, setToday] = useState(moment().format("YYYY-MM-DD"));
+  const [mood, setMood] = useState("")
+  const [anxiety, setAnxiety] = useState("")
 
   const calculateDay = (start: string) => {
     const startDate = moment(start);
@@ -25,7 +27,45 @@ const MoodForm = () => {
             ></input>
           </div>
         </label>
+        <div style={styles.formInput}>
         {start && <div> Day {calculateDay(start)} </div>}
+        </div>
+        <div style={styles.formInput}>
+        <label>
+          How would you rate your mood?
+          <select value={mood} onChange={(e) => setMood(e.target.value)}>
+            <option></option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+            <option value={9}>9</option>
+            <option value={10}>10</option>
+          </select>
+        </label>
+        </div>
+        <div style={styles.formInput}>
+        <label>
+          How would you rate your anxiety?
+          <select value={anxiety} onChange={(e) => setAnxiety(e.target.value)}>
+            <option></option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+            <option value={9}>9</option>
+            <option value={10}>10</option>
+          </select>
+        </label>
+        </div>
       </form>
     </div>
   );
