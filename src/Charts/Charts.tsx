c5219d4506cf7162be78ed01ca6544e1cd419821
Amelia Schulz <amelia.schulz@gmail.com>
2020-11-06 17:48:11 -0500

    Add customized x axes on dummy chart.

--- src/Charts/Charts.tsx
+++ src/Charts/Charts.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { VictoryBar, VictoryChart } from 'victory'
+import { VictoryAxis, VictoryBar, VictoryChart } from 'victory'
 
 const data = [
   { day: 1, mood: 3 },
@@ -8,11 +8,16 @@ const data = [
   { day: 4, mood: 6 },
 ]
 
+const xValues = data.map((point) => {
+  return point.day
+})
+
 const Charts = () => {
   return (
     <div>
-      <VictoryChart>
-        <VictoryBar data={data} x="day" y="mood" />
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis tickValues={xValues} tickFormat={(x) => (`Day ${x}`)} />
+        <VictoryBar data={data} x='day' y='mood' />
       </VictoryChart>
     </div>
   )
